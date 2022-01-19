df99d0e1f9755b33126b5764f217eeb57017a452
Guilherme Lourenço da Silva <guilhermesilva@acad.ifma.edu.br>
2022-01-19 08:47:48 -0300

    working

---
+++ typeScript/src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,16 @@
+// import { Specification } from '../models/Specification';
+
+import { Specification } from '../models/Specification';
+
+interface ICreateSpecificationDTO {
+  name: string;
+  description: string;
+}
+
+interface ISpecificationRepository {
+  create({ name, description }: ICreateSpecificationDTO): void;
+  list(): Specification[];
+  findByName(name: string): Specification;
+}
+
+export { ISpecificationRepository, ICreateSpecificationDTO };
